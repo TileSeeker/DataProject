3ff08afe1f3893268bf9abba6d5fd08f77c84975
Sveien <75600464+Sveien@users.noreply.github.com>
2021-05-11 15:40:10 +0200

    oppdatering

--- sikringsskap_data.py
+++ sikringsskap_data.py
@@ -36,29 +36,24 @@ key_6 = "15853"
 
 #excel headers
 headers = ("Day/hour", "Wh_1", "Wh_2", "Wh_3", "Wh_4", "Wh_5", "Wh_6", 
-            "t_kWh_1", "t_kWh_2", "t_kWh_3", "t_kWh_4", "t_kWh_5", "t_kWh_6")
+            "ore_1", "ore_2", "ore_3", "ore_4", "ore_5", "ore_6", 
+            "t_kWh_1", "t_kWh_2", "t_kWh_3", "t_kWh_4", "t_kWh_5", "t_kWh_6",
+            "t_NOK_1","t_NOK_2","t_NOK_3","t_NOK_4","t_NOK_5","t_NOK_6",)
 
-
-
+#1 gang i timen
 def kWh_to_NOK_upload():
-    dagens_strømpris = 26.55
-    #dagens_strømpris = strømpris("D:\.drivere\geckodriver.exe")
-    #dagens_strømpris = dagens_strømpris.get()
-
     todays_date = date.today()
     current_date = str(todays_date.year) + "-" + str(todays_date.month)
     
-    wb = openpyxl.load_workbook("strømdata.xlsx")
-    sh = wb[current_date]
-    wb = openpyxl.load_workbook("strømdata.xlsx", data_only=True)
+    wb = openpyxl.load_workbook("strømbruk.xlsx", data_only=True)
     sh = wb[current_date]
 
-    NOK_1 =((sh.cell(2,8).value)*dagens_strømpris)*0.01
-    NOK_2 =((sh.cell(2,9).value)*dagens_strømpris)*0.01
-    NOK_3 =((sh.cell(2,10).value)*dagens_strømpris)*0.01
-    NOK_4 =((sh.cell(2,11).value)*dagens_strømpris)*0.01
-    NOK_5 =((sh.cell(2,12).value)*dagens_strømpris)*0.01
-    NOK_6 =((sh.cell(2,13).value)*dagens_strømpris)*0.01
+    NOK_1 =sh.cell(2,20).value
+    NOK_2 =sh.cell(2,21).value
+    NOK_3 =sh.cell(2,22).value
+    NOK_4 =sh.cell(2,23).value
+    NOK_5 =sh.cell(2,24).value
+    NOK_6 =sh.cell(2,25).value
     
     Signal(key_1, token1).write(NOK_1)
     Signal(key_2, token2).write(NOK_2)
@@ -66,8 +61,9 @@ def kWh_to_NOK_upload():
     Signal(key_4, token4).write(NOK_4)
     Signal(key_5, token5).write(NOK_5)
     Signal(key_6, token6).write(NOK_6)
+    wb.save("strømbruk.xlsx")
 
-
+#1 gang i timen
 def get_store_data():
     #get data from cot
     signal1 = Signal(key_rom1, token).get()
@@ -77,19 +73,30 @@ def get_store_data():
     signal5 = Signal(key_rom5, token).get()
     signal6 = Signal(key_rom6, token).get()
     time_now = datetime.now().strftime("%d %H:%M:%S")
-    strømlist = (time_now, signal1, signal2, signal3, signal4, signal5, signal6)
+    #dagens_strømpris = strømpris("D:\.drivere\geckodriver.exe")
+    #dagens_strømpris = dagens_strømpris.get()
+    dagens_strømpris = 22.5 # ore/kWh
+    #strømpris*wh*10
+    ore1 = (signal1*0.001)*dagens_strømpris
+    ore2 = (signal2*0.001)*dagens_strømpris
+    ore3 = (signal3*0.001)*dagens_strømpris
+    ore4 = (signal4*0.001)*dagens_strømpris
+    ore5 = (signal5*0.001)*dagens_strømpris
+    ore6 = (signal6*0.001)*dagens_strømpris
+
+    strømlist = (time_now, signal1, signal2, signal3, signal4, signal5, signal6, ore1, ore2, ore3, ore4, ore5, ore6)
 
     #store data in excel
-    wb = openpyxl.load_workbook("strømdata.xlsx")
+    wb = openpyxl.load_workbook("strømbruk.xlsx")
     todays_date = date.today()
     current_date = str(todays_date.year) + "-" + str(todays_date.month)
     sh = wb[current_date]
     sh.append(strømlist)
-    wb.save("strømdata.xlsx")
+    wb.save("strømbruk.xlsx")
 
-#creating datasheet for 1 month
+#creating datasheet for 1 month # 1 gang i måneden
 def create_datasheet():
-    wb = openpyxl.load_workbook("strømdata.xlsx")
+    wb = openpyxl.load_workbook("strømbruk.xlsx")
     todays_date = date.today()
     current_date = str(todays_date.year) + "-" + str(todays_date.month)
     wb.create_sheet(current_date)
@@ -97,11 +104,23 @@ def create_datasheet():
     sh.append(headers)
 
     #sum for each person in kWh
-    sh["N1"] = "0,001"
-    sh["H2"] = "=SUM(B2:B747)*N1"
-    sh["I2"] = "=SUM(C2:C747)*N1"
-    sh["J2"] = "=SUM(D2:D747)*N1"
-    sh["K2"] = "=SUM(E2:E747)*N1"
-    sh["L2"] = "=SUM(F2:F747)*N1"
-    sh["M2"] = "=SUM(G2:G747)*N1"
-    wb.save("strømdata.xlsx")
+    sh["Z1"] = "0,001"
+    sh["N2"] = "=SUM(B2:B747)*Z1"
+    sh["O2"] = "=SUM(C2:C747)*Z1"
+    sh["P2"] = "=SUM(D2:D747)*Z1"
+    sh["Q2"] = "=SUM(E2:E747)*Z1"
+    sh["R2"] = "=SUM(F2:F747)*Z1"
+    sh["S2"] = "=SUM(G2:G747)*Z1"
+    
+    #sum for each person in NOK
+    sh["AA1"] = "0,01"
+    sh["T2"] = "=SUM(H2:H747)*AA1"
+    sh["U2"] = "=SUM(I2:I747)*AA1"
+    sh["V2"] = "=SUM(J2:J747)*AA1"
+    sh["W2"] = "=SUM(K2:K747)*AA1"
+    sh["X2"] = "=SUM(L2:L747)*AA1"
+    sh["Y2"] = "=SUM(M2:M747)*AA1"
+
+
+    wb.save("strømbruk.xlsx")
+    
---
+++ time_update.py
@@ -0,0 +1,17 @@
+import time
+import datetime
+from Signals import Signal
+
+token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1NjcwIn0.4GshD9I6ZBE0roZzIsjHpIBLasIbH0JLc3TRhJwxJg8"
+
+key_time = "20723"
+key_date = "6660"
+
+#1 gang hver min
+def time_min():
+    time_now = int(currentTime.strftime("%H%M"))
+    Signal(key_time, token).write(time_now)
+#1 gang om dagen
+def year_month_day():
+    date_now = int(currentTime.strftime("%Y%m%d"))
+    Signal(key_date, token).write(date_now)
